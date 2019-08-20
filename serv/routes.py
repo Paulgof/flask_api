6f224b7f50e780fb5e84f5bbb2d22a3406060cd5
Kamiex Void <paulgof@outlook.com>
2019-08-20 21:21:15 +0400

    Fork: psql JSONB model

--- serv/routes.py
+++ serv/routes.py
@@ -1,1 +1,28 @@
+from flask import abort, request, jsonify
+from serv import app
+from serv.models import Data
 
+
+@app.route('/imports', methods=['POST'])
+def set_import():
+    data = request.get_json()
+    return jsonify(data)
+
+@app.route('/imports/<int:import_id>/citizens/<int:citizen_id>', methods=['PATCH'])
+def patch_user(import_id, citizen_id):
+    pass
+
+
+@app.route('/imports/<int:import_id>/citizens', methods=['GET'])
+def get_users(import_id):
+    pass
+
+
+@app.route('/imports/<int:import_id>/citizens/birthdays', methods=['GET'])
+def get_birthdays(import_id):
+    pass
+
+
+@app.route('/imports/<int:import_id>/towns/stat/percentile/age', methods=['GET'])
+def get_percentile(import_id):
+    pass
